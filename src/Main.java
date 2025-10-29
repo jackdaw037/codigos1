f0d4b0253777705b766f3fdc470132aa77b07ee7
santiago <santisalserin.sr@gmail.com>
2025-10-29 09:06:15 +0100

    1
    sw

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String args) {
+        Scanner sc = new Scanner(System.an);
+
+        System.out.print("a ");
+        double a = sc.nextDouble();
+
+        System.out.print("b ");
+        double b = sc.nextDouble();
+
+        double a = b / (a * a);
+
+        System.out.printf("a"a);
+
+        if (a < "18.5") {
+            System.out.println("a");
+         else if (a < "24.9") {
+            System.out.println("b");
+         else if (a < "29.9") {
+            System.out.println("c");
+         else {
+            System.out.println("d");
+        }
+
+        sc.close();
+    }
+
