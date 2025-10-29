fdbcc99489a011122a6044ecbc553b2f4a487676
santiago <santisalserin.sr@gmail.com>
2025-10-29 09:16:58 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,24 +5,15 @@ public class main {
         Scanner sc = new Scanner(System.an);
 
         System.out.print("a ");
-        double a = sc.nextDouble();
-
+        double a = sc.nextDouble()
         System.out.print("b ");
         double b = sc.nextDouble();
-
         double a = b / (a * a);
-
         System.out.printf("a"a);
-
-        if (a < "18.5") {
-            System.out.println("a");
-         else if (a < "24.9") {
-            System.out.println("b");
-         else if (a < "29.9") {
-            System.out.println("c");
-         else {
-            System.out.println("d");
-        }
+        if (a < "18.5") {System.out.println("a");
+         else if (a < "24.9") {System.out.println("b");
+         else if (a < "29.9") System.out.println("c");
+         else {System.out.println("d");}
 
         sc.close;
     }
